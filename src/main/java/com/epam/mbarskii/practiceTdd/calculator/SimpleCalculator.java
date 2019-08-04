d6c693d107ee8a48ab071935ba48f7d7739f5d9c
Mike Barskiy <mikebarskiy@gmail.com>
2019-08-04 13:57:22 +0300

    Red tests was added

--- src/main/java/com/epam/mbarskii/practiceTdd/calculator/SimpleCalculator.java
+++ src/main/java/com/epam/mbarskii/practiceTdd/calculator/SimpleCalculator.java
@@ -1,23 +1,24 @@
 package com.epam.mbarskii.practiceTdd.calculator;
 
 public class SimpleCalculator implements Calculator {
+
     @Override
     public double sum(double x, double y) {
-        return 0;
+        return x + y;
     }
 
     @Override
     public double subtract(double x, double y) {
-        return 0;
+        return x - y;
     }
 
     @Override
     public double divide(double x, double y) {
-        return 0;
+        return x / y;
     }
 
     @Override
     public double multiply(double x, double y) {
-        return 0;
+        return x * y;
     }
 }
--- src/test/java/com/epam/mbarskii/calculator/SimpleCalculatorTest.java
+++ src/test/java/com/epam/mbarskii/calculator/SimpleCalculatorTest.java
@@ -9,30 +9,59 @@ import static junit.framework.TestCase.assertEquals;
 public class SimpleCalculatorTest {
 
     @Test
-    public void checkCalculatorSum() {
+    public void checkSumOfIntegerValues() {
         Calculator calculator = new SimpleCalculator();
         double result = calculator.sum(8, 4);
-        assertEquals(result, 12);
+        assertEquals(result, 12d);
     }
 
     @Test
-    public void checkCalculatorSubtract() {
+    public void checkSumOfDoubleValues() {
+        Calculator calculator = new SimpleCalculator();
+        double result = calculator.sum(8.15, 4.15);
+        assertEquals(result, 12.3);
+    }
+
+    @Test
+    public void checkSubtractOfIntegerValues() {
         Calculator calculator = new SimpleCalculator();
         double result = calculator.subtract(8, 4);
-        assertEquals(result, 4);
+        assertEquals(result, 4d);
     }
 
     @Test
-    public void checkCalculatorDivide() {
+    public void checkSubtractOfDoubleValues() {
+        Calculator calculator = new SimpleCalculator();
+        double result = calculator.subtract(8.15, 4.05);
+        assertEquals(result, 4.1);
+    }
+
+    @Test
+    public void checkDivideOfIntegerValues() {
         Calculator calculator = new SimpleCalculator();
         double result = calculator.divide(8, 4);
-        assertEquals(result, 2);
+        assertEquals(result, 2d);
     }
 
     @Test
-    public void checkCalculatorMultiply() {
+    public void checkDivideOfDoubleValues() {
+        Calculator calculator = new SimpleCalculator();
+        double result = calculator.divide(8.12, 4.01);
+        assertEquals(result, 2.02493765586);
+    }
+
+    @Test
+    public void checkMultiplyOfIntegerValues() {
         Calculator calculator = new SimpleCalculator();
         double result = calculator.multiply(8, 4);
-        assertEquals(result, 32);
+        assertEquals(result, 32d);
     }
+
+    @Test
+    public void checkMultiplyOfDoubleValues() {
+        Calculator calculator = new SimpleCalculator();
+        double result = calculator.multiply(8.12, 4.01);
+        assertEquals(result, 32.5612);
+    }
+
 }
