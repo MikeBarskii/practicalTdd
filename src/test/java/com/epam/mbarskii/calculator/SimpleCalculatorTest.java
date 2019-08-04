6f6b85ae95190b4a0a2756e6321825072aeb129c
Mike Barskiy <mikebarskiy@gmail.com>
2019-08-04 14:55:20 +0300

    Refactoring tests

--- src/test/java/com/epam/mbarskii/calculator/SimpleCalculatorTest.java
+++ src/test/java/com/epam/mbarskii/calculator/SimpleCalculatorTest.java
@@ -2,6 +2,7 @@ package com.epam.mbarskii.calculator;
 
 import com.epam.mbarskii.practiceTdd.calculator.Calculator;
 import com.epam.mbarskii.practiceTdd.calculator.SimpleCalculator;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -11,58 +12,57 @@ import static junit.framework.TestCase.assertEquals;
 
 public class SimpleCalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setup() {
+        calculator = new SimpleCalculator();
+    }
+
     @Test
     public void checkSumOfIntegerValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.sum(valueOf(8), valueOf(4));
         assertEquals(valueOf(12), result);
     }
 
     @Test
     public void checkSumOfDoubleValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.sum(valueOf(8.15), valueOf(4.15));
         assertEquals(valueOf(12.3), result);
     }
 
     @Test
     public void checkSubtractOfIntegerValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.subtract(valueOf(8), valueOf(4));
         assertEquals(valueOf(4), result);
     }
 
     @Test
     public void checkSubtractOfDoubleValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.subtract(valueOf(8.15), valueOf(4.05));
         assertEquals(valueOf(4.1), result);
     }
 
     @Test
     public void checkDivideOfIntegerValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.divide(valueOf(8), valueOf(4));
         assertEquals(valueOf(2), result);
     }
 
     @Test
     public void checkDivideOfDoubleValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.divide(valueOf(8.12), valueOf(4.01));
         assertEquals(valueOf(2.02493765586), result);
     }
 
     @Test
     public void checkMultiplyOfIntegerValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.multiply(valueOf(8), valueOf(4));
         assertEquals(valueOf(32), result);
     }
 
     @Test
     public void checkMultiplyOfDoubleValues() {
-        Calculator calculator = new SimpleCalculator();
         BigDecimal result = calculator.multiply(valueOf(8.12), valueOf(4.01));
         assertEquals(valueOf(32.5612), result);
     }
